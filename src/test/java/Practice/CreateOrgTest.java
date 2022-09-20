06475d7e2307b4f3be85a4782e3aa736dc4296b0
admin <pranalikharpude01@gmail.com>
2022-09-20 21:25:47 +0530

    Commit 1-Eng 1

---
+++ src/test/java/Practice/CreateOrgTest.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createNewOrg()
+	{
+		System.out.println("This is Create New org-commit 1");
+	}
+	
+	
+}
