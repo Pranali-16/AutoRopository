c14fc0d66fd08e48446d5cdf546c8d29ef82b6a9
admin <admin@DESKTOP-8NEVVBR>
2022-09-20 22:22:35 +0530

    Commit 3-Eng 1

---
+++ src/test/java/Practice/CreateContactTest.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test 
+	public void createContact()
+	{
+		System.out.println("This is Create New org-commit 3");
+	}
+	
+	
+	
+}
